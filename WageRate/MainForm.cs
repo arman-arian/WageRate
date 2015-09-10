5713f33052a6fe84c12ac27efd5f98d4669763ff
arman_arian <arman.hassanpoor2000@gmail.com>
2015-09-10 17:16:04 +0430

    Resolve Variables

--- WageRate/MainForm.cs
+++ WageRate/MainForm.cs
@@ -15,20 +15,20 @@ namespace WageRate
         {
             txtLog.Text = string.Empty;
 
-            var y0 = decimal.Parse(txtAmount.Text);
-            var d = decimal.Parse(txtDuration.Text);
+            var amount = decimal.Parse(txtAmount.Text);
+            var duration = decimal.Parse(txtDuration.Text);
             var rate = decimal.Parse(txtRate.Text);
-            var installment = decimal.Parse(txtInstal.Text);
+            var installmentAmount = decimal.Parse(txtInstal.Text);
 
-            for (var i = 1; i <= d; i++)
+            for (var i = 1; i <= duration; i++)
             {
-                var s = ((y0*rate*12)/1200)/12;
-                y0 = y0 + s - installment;
+                var s = ((amount * rate * 12) / 1200) / 12;
+                amount = amount + s - installmentAmount;
 
-                txtLog.Text += @"سود ماه " + i + @" : " + Math.Floor(s - installment) + @" مانده: " + Math.Floor(y0) + Environment.NewLine;
+                txtLog.Text += @"سود ماه " + i + @" : " + Math.Floor(s - installmentAmount) + @" مانده: " + Math.Floor(amount) + Environment.NewLine;
             }
 
-            txtFinalAmount.Text = Math.Floor(y0).ToString(CultureInfo.InvariantCulture);
+            txtFinalAmount.Text = Math.Floor(amount).ToString(CultureInfo.InvariantCulture);
         }
     }
 }
